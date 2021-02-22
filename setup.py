1a93e3a764f93df005efa72b95c1028d0a77faf5
Orsiris de Jong <ozy@netpower.fr>
2021-02-22 12:24:06 +0100

    Refactor setup.py file

--- setup.py
+++ setup.py
@@ -4,12 +4,23 @@
 # This file is part of command_runner package
 
 
+__intname__ = 'command_runner.setup'
+__author__ = 'Orsiris de Jong'
+__copyright__ = 'Copyright (C) 2021 Orsiris de Jong'
+__licence__ = 'BSD 3 Clause'
+__build__ = '2021022201'
+
 import codecs
 import os
-import shutil
 
 import pkg_resources
 import setuptools
+
+
+def _read_file(filename):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, filename), 'r', encoding='utf-8') as file_handle:
+        return file_handle.read()
 
 
 def get_metadata(package_file):
@@ -17,20 +28,12 @@ def get_metadata(package_file):
     Read metadata from package file
     """
 
-    def _read(_package_file):
-        here = os.path.abspath(os.path.dirname(__file__))
-        with codecs.open(os.path.join(here, _package_file), 'r') as fp:
-            return fp.read()
-
     _metadata = {}
 
-    for line in _read(package_file).splitlines():
-        if line.startswith('__version__'):
-            delim = '"' if '"' in line else "'"
-            _metadata['version'] = line.split(delim)[1]
-        if line.startswith('__description__'):
-            delim = '"' if '"' in line else "'"
-            _metadata['description'] = line.split(delim)[1]
+    for line in _read_file(package_file).splitlines():
+        if line.startswith('__version__') or line.startswith('__description__'):
+            delim = '='
+            _metadata[line.split(delim)[0].strip().strip('__')] = line.split(delim)[1].strip().strip('\'"')
     return _metadata
 
 
@@ -40,32 +43,24 @@ def parse_requirements(filename):
     Let's build a simple one
     """
     try:
-        with open(filename, 'r') as requirements_txt:
-            install_requires = [
-                str(requirement)
-                for requirement
-                in pkg_resources.parse_requirements(requirements_txt)
-            ]
+        requirements_txt = _read_file(filename)
+        install_requires = [
+            str(requirement)
+            for requirement
+            in pkg_resources.parse_requirements(requirements_txt)
+        ]
         return install_requires
     except OSError:
         print('WARNING: No requirements.txt file found as "{}". Please check path or create an empty one'
               .format(filename))
 
 
-def get_long_description(filename):
-    with open(filename, 'r', encoding='utf-8') as readme_file:
-        _long_description = readme_file.read()
-    return _long_description
-
-with open('README.md', 'r', encoding='utf-8') as readme_file:
-    long_description = readme_file.read()
-
-
 PACKAGE_NAME = 'command_runner'
 package_path = os.path.abspath(PACKAGE_NAME)
 package_file = os.path.join(package_path, '__init__.py')
 metadata = get_metadata(package_file)
-#requirements = parse_requirements(os.path.join(package_path, 'requirements.txt'))
+requirements = parse_requirements(os.path.join(package_path, 'requirements.txt'))
+long_description = _read_file('README.md')
 
 setuptools.setup(
     name='command_runner',
