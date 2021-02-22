9332a0cef37b0b7abdc54d917b6491e4e47c0179
Orsiris de Jong <ozy@netpower.fr>
2021-02-22 11:49:23 +0100

    Reformat file

--- tests/test_command_runner.py
+++ tests/test_command_runner.py
@@ -18,21 +18,18 @@ __intname__ = 'command_runner_tests'
 __author__ = 'Orsiris de Jong'
 __copyright__ = 'Copyright (C) 2015-2021 Orsiris de Jong'
 __licence__ = 'BSD 3 Clause'
-__build__ = '2021022101'
+__build__ = '2021022201'
 
-
-import os
-from time import sleep
 from command_runner import *
-
 
 if os.name == 'nt':
     ENCODING = 'cp437'
     PING_CMD = 'ping 127.0.0.1 -n 4'
 else:
     ENCODING = 'utf-8'
-    PING_CMD = ['ping' , '127.0.0.1', '-c', '4']
+    PING_CMD = ['ping', '127.0.0.1', '-c', '4']
     # TODO shlex.split(command, posix=True) test for Linux
+
 
 def test_standard_ping_with_encoding():
     """
@@ -41,6 +38,7 @@ def test_standard_ping_with_encoding():
     exit_code, output = command_runner(PING_CMD, encoding=ENCODING)
     print(output)
     assert exit_code == 0, 'Exit code should be 0 for ping command'
+
 
 def test_timeout():
     """
@@ -51,6 +49,7 @@ def test_timeout():
     assert exit_code == -254, 'Exit code should be -254 on timeout'
     assert 'Timeout' in output, 'Output should have timeout'
 
+
 def test_no_timeout():
     """
     Test with setting timeout=None
@@ -59,12 +58,14 @@ def test_no_timeout():
     print(output)
     assert exit_code == 0, 'Without timeout, command should have run'
 
+
 def test_live_output():
     """
     Test command_runner with live output to stdout
     """
     exit_code, _ = command_runner(PING_CMD, stdout=PIPE, encoding=ENCODING)
     assert exit_code == 0, 'Exit code should be 0 for ping command'
+
 
 def test_not_found():
     """
@@ -73,6 +74,7 @@ def test_not_found():
     print('The following command should fail')
     exit_code, _ = command_runner('unknown_command_nowhere_to_be_found_1234')
     assert exit_code == -253, 'Unknown command should trigger a -253 exit code'
+
 
 def test_file_output():
     """
@@ -91,12 +93,14 @@ def test_file_output():
     os.remove(stdout_filename)
     os.remove(stderr_filename)
 
+
 def test_valid_exit_codes():
     """
     Test command_runner with a failed ping but that should not trigger an error
     """
     exit_code, _ = command_runner('ping nonexistent_host', shell=True, valid_exit_codes=[0, 1, 2])
     assert exit_code in [0, 1, 2], 'Exit code not in valid list'
+
 
 def test_unix_only_split_command():
     """
@@ -113,7 +117,8 @@ def test_create_no_window():
     """
     if os.name == 'nt':
         exit_code, _ = command_runner(PING_CMD, windows_no_window=True)
-        assert exit_code ==0, 'Should have worked too'
+        assert exit_code == 0, 'Should have worked too'
+
 
 def test_deferred_command():
     """
