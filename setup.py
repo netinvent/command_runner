1ac3662a4203eb3b57b7bc8ccf56faa5cf6b328e
Orsiris de Jong <ozy@netpower.fr>
2021-02-22 12:39:28 +0100

    Fix long description was not properly shown on PyPI

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ __copyright__ = 'Copyright (C) 2021 Orsiris de Jong'
 __licence__ = 'BSD 3 Clause'
 __build__ = '2021022201'
 
-import codecs
 import os
 
 import pkg_resources
@@ -19,7 +18,7 @@ import setuptools
 
 def _read_file(filename):
     here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, filename), 'r', encoding='utf-8') as file_handle:
+    with open(os.path.join(here, filename), 'r', encoding='utf-8') as file_handle:
         return file_handle.read()
 
 
