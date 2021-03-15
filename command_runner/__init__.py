8fd87f5dc7afd5966abbefe81a1cff8f35ecd184
Orsiris de Jong <ozy@netpower.fr>
2021-03-15 16:01:06 +0100

    Bump version

--- command_runner/__init__.py
+++ command_runner/__init__.py
@@ -18,8 +18,8 @@ __intname__ = 'command_runner'
 __author__ = 'Orsiris de Jong'
 __copyright__ = 'Copyright (C) 2015-2021 Orsiris de Jong'
 __licence__ = 'BSD 3 Clause'
-__version__ = '0.6.2'
-__build__ = '2021022202'
+__version__ = '0.6.3'
+__build__ = '2021031501'
 
 import os
 import shlex
