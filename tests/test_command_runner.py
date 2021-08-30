c8514958725da8b072dd529e48772eaf47b720cb
Orsiris de Jong <ozy@netpower.fr>
2021-08-30 16:10:33 +0200

    Update test_command_runner.py

--- tests/test_command_runner.py
+++ tests/test_command_runner.py
@@ -18,9 +18,8 @@ __intname__ = 'command_runner_tests'
 __author__ = 'Orsiris de Jong'
 __copyright__ = 'Copyright (C) 2015-2021 Orsiris de Jong'
 __licence__ = 'BSD 3 Clause'
-__build__ = '2021081901'
+__build__ = '2021083001'
 
-from time import sleep
 from command_runner import *
 
 if os.name == 'nt':
