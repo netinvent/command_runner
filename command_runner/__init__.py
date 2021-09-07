362ad1a54c1287c4f19c513c38b27dfefdcc37bb
Orsiris de Jong <ozy@netpower.fr>
2021-09-07 13:34:05 +0200

    Bump version

--- command_runner/__init__.py
+++ command_runner/__init__.py
@@ -18,8 +18,8 @@ __intname__ = "command_runner"
 __author__ = "Orsiris de Jong"
 __copyright__ = "Copyright (C) 2015-2021 Orsiris de Jong"
 __licence__ = "BSD 3 Clause"
-__version__ = "1.1.0"
-__build__ = "2021090601"
+__version__ = "1.2.0"
+__build__ = "2021090701"
 
 import io
 import os
