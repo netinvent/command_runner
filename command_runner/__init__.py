a6e827568814303aa7c53438094344dc59e5aa5e
deajan <ozy@netpower.fr>
2025-03-10 10:04:19 +0100

    Bump version

--- command_runner/__init__.py
+++ command_runner/__init__.py
@@ -19,10 +19,10 @@ from __future__ import unicode_literals
 
 __intname__ = "command_runner"
 __author__ = "Orsiris de Jong"
-__copyright__ = "Copyright (C) 2015-2024 Orsiris de Jong for NetInvent SASU"
+__copyright__ = "Copyright (C) 2015-2025 Orsiris de Jong for NetInvent"
 __licence__ = "BSD 3 Clause"
-__version__ = "1.7.0"
-__build__ = "2024091501"
+__version__ = "1.7.1"
+__build__ = "2025031001"
 __compat__ = "python2.7+"
 
 import io
